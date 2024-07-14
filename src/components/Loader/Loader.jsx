57d3ea7a5d9544ae87122f925717214963dc8505
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-07-14 19:50:15 +0800

    refactor: replace react-loader-spinner
    - Removed react-loader-spinner dependency.
    - Added react-spinners dependency.
    - Updated Loader component to use react-spinners for displaying loading animations.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,6 @@
-import { RotatingLines } from 'react-loader-spinner';
+import { SyncLoader } from 'react-spinners';
 import css from './Loader.module.css';
 
 export const Loader = () => {
-  return (
-    <div className={css.loaderWrap}>
-      <RotatingLines
-        visible={true}
-        height='96'
-        width='96'
-        color='grey'
-        strokeWidth='5'
-        animationDuration='0.75'
-        ariaLabel='rotating-lines-loading'
-        wrapperStyle={{}}
-        wrapperClass=''
-      />
-    </div>
-  );
+  return <SyncLoader className={css.loader} color='#3f51b5' />;
 };
